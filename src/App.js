4a9fb8debcf7cf6383b553ad69f9250feae12b1e
dbeccaria826 <beccdaniel@gmail.com>
2020-04-18 09:07:35 -0400

    added button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from './Button';
 
 const App = () => {
     return (
         <div>
             <h1>Default App</h1>
+            <Button />
         </div>
     ) 
 }

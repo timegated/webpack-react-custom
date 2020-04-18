1e27c5bce4c8716493244cb335d112a17bad3870
dbeccaria826 <beccdaniel@gmail.com>
2020-04-18 09:04:43 -0400

    added setup for a minimal react app

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/App.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({template: "./src/index.html"})]
+}
